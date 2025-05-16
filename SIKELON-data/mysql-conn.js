a76069cc82c106984da088f788c6869ae2d6f3be
Ba3yoo <115632689+Ba3yoo@users.noreply.github.com>
2025-05-16 14:11:39 +0700

    3

--- SIKELON-data/mysql-conn.js
+++ SIKELON-data/mysql-conn.js
@@ -2,13 +2,13 @@ var mysql = require('mysql');
 var fs = require('fs');
 var readline = require('readline');
 
-var con1 = mysql.createConnection({
+var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
 });
 
-con1.connect(function(err) {
+con.connect(function(err) {
     if (err) throw err;
     console.log("Connected 1!");
 });
@@ -18,21 +18,17 @@ var rl = readline.createInterface({
   terminal: false
  });
 rl.on('line', function(chunk){
-    con1.query(chunk.toString('ascii'), function(err, sets, fields){
+    con.query(chunk.toString('ascii'), function(err, sets, fields){
      if(err) console.log(err);
     });
 });
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
+con.changeUser({
   database: "sikelon"
-});
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected 2!");
+}, function (err) {
+  if (err) {
+    console.log('change error')
+  }
 });
 
 module.exports = {

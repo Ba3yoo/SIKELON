4fa332b4b0a012e8449f56d3643f22ae6506c959
Riyan <153371196+riyannnnnih@users.noreply.github.com>
2025-05-16 12:19:06 +0700

    added navigation usage

--- app/src/main/java/com/rati/sikelon/MainActivity.kt
+++ app/src/main/java/com/rati/sikelon/MainActivity.kt
@@ -1,48 +1,48 @@
-package com.rati.sikelon
+package com.example.sikelon
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.rati.sikelon.ui.theme.SIKELONTheme
-import com.rati.sikelon.view.cart.Cart
+import androidx.navigation.compose.rememberNavController
+import com.example.sikelon.navigate.AppNavHost
+import com.example.sikelon.ui.theme.SIKELONTheme
+import com.example.sikelon.viewmodel.ViewModel
 
 class MainActivity : ComponentActivity() {
+    private val userViewModel: ViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             SIKELONTheme {
-                Surface (
+                Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Cart()
+                    val navController = rememberNavController()
+                    val users by userViewModel.users.collectAsState()
+
+                    LaunchedEffect(Unit) {
+                        userViewModel.loadUsers()
+                    }
+
+//                    AppNavHost(
+//                        navController = navController,
+//                        userViewModel = userViewModel
+//                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SIKELONTheme {
-        Greeting("Android")
     }
 }

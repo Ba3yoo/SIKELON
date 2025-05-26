1690f6401dcc8d7884d75e34c7383b16d9f456a6
Muhammad Rifqi Taufan <113281902+RifqiTaufan@users.noreply.github.com>
2025-05-26 08:42:43 +0700

    add 1 profile settings page

---
+++ app/src/main/java/com/rati/sikelon/view/profile/ProfileSettings.kt
@@ -0,0 +1,195 @@
+package com.rati.sikelon.view.profile
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rati.sikelon.R
+
+@Composable
+fun ProfileSettings(onBackClick: () -> Unit) {
+    var password by remember { mutableStateOf("") }
+    var new by remember { mutableStateOf("") }
+    var confirm by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
+    var newVisible by remember { mutableStateOf(false) }
+    var confirmVisible by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Ubah Password",
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold
+            )
+            IconButton(
+                onClick = onBackClick,
+                modifier = Modifier.align(Alignment.CenterStart)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.back_sikelon),
+                    contentDescription = "Kembali",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            text = "Password",
+            fontWeight = FontWeight.SemiBold,
+            color = Color(0xFF7E60BF),
+            modifier = Modifier.align(Alignment.Start)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            placeholder = { Text("Masukkan password Anda di sini", color = Color(0xFF7E60BF))},
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                    val iconRes = if (passwordVisible) R.drawable.visible else R.drawable.hide
+                    Image(
+                        painter = painterResource(id = iconRes),
+                        contentDescription = if (passwordVisible) "Sembunyikan Password" else "Tampilkan password",
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+            },
+            shape = RoundedCornerShape(50),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color(0xFF7E60BF),
+                unfocusedBorderColor = Color(0xFFBDBDBD)
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            text = "Password Baru",
+            fontWeight = FontWeight.SemiBold,
+            color = Color(0xFF7E60BF),
+            modifier = Modifier.align(Alignment.Start)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = new,
+            onValueChange = { new = it },
+            placeholder = { Text("Masukkan password baru Anda", color = Color(0xFF7E60BF))},
+            visualTransformation = if (newVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                IconButton(onClick = { newVisible = !newVisible }) {
+                    val iconRes = if (newVisible) R.drawable.visible else R.drawable.hide
+                    Image(
+                        painter = painterResource(id = iconRes),
+                        contentDescription = if (newVisible) "Sembunyikan Password" else "Tampilkan password",
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+            },
+            shape = RoundedCornerShape(50),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color(0xFF7E60BF),
+                unfocusedBorderColor = Color(0xFFBDBDBD)
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            text = "Confirm Password",
+            fontWeight = FontWeight.SemiBold,
+            color = Color(0xFF7E60BF),
+            modifier = Modifier.align(Alignment.Start)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = confirm,
+            onValueChange = { confirm = it },
+            placeholder = { Text("Konfirmasi password baru Anda", color = Color(0xFF7E60BF))},
+            visualTransformation = if (confirmVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                IconButton(onClick = { confirmVisible = !confirmVisible }) {
+                    val iconRes = if (confirmVisible) R.drawable.visible else R.drawable.hide
+                    Image(
+                        painter = painterResource(id = iconRes),
+                        contentDescription = if (confirmVisible) "Sembunyikan Password" else "Tampilkan password",
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+            },
+            shape = RoundedCornerShape(50),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color(0xFF7E60BF),
+                unfocusedBorderColor = Color(0xFFBDBDBD)
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(
+            onClick = {},
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Ubah")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewProfileSettings() {
+    ProfileSettings(onBackClick = {})
+}
